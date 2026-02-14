ed2222c5c2bb95392c4d12cf5e41d7b681d35ac1
Yoniwh <yoniwh@gmail.com>
2026-02-14 12:33:56 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Admatenu Betenu Chatbot is running!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
